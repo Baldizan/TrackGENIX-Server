a623c10c541a88ec6b2fa8765057136f95986275
Elias Bermudez <eliasbermudezxiii@gmail.com>
2022-10-08 19:12:23 -0300

    TG: getting projects filtered by id

--- src/resources/projects.js
+++ src/resources/projects.js
@@ -3,9 +3,22 @@ const express = require('express');
 const router = express.Router();
 const projects = require('../data/projects.json');
 
-//  Get All porjects list
+//  Get All projects list
 router.get('/getAll', (req, res) => {
   res.send(projects);
+});
+
+//  Get projects by id
+router.get('/getById/:id', (req, res) => {
+  const projectId = parseInt(req.params.id, 10);
+  const foundProject = projects.find((project) => project.id === projectId);
+  if (foundProject) {
+    res.send(foundProject);
+  } else {
+    res
+      .status(400)
+      .json({ msg: `There is no project with the id of ${req.params.id}` });
+  }
 });
 
 module.exports = router;
