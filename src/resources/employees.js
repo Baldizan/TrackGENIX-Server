5305450109357ce2a0275a8cd641b90721f84292
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-10-09 00:11:43 -0300

    TG: create delete method

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
+const fs = require('fs');
 const employee = require('../data/employees.json');
 
 router.get('/getEmployee/:id', (req, res) => {
@@ -10,6 +11,23 @@ router.get('/getEmployee/:id', (req, res) => {
     res.send(findEmployee);
   } else {
     res.send('User not found.');
+  }
+});
+
+router.delete('/deleteEmployee/:id', (req, res) => {
+  const employeeId = req.params.id;
+  const findEmployee = employee.find((emp) => emp.id === employeeId);
+  if (findEmployee) {
+    const filteredEmployee = employee.filter((emp) => emp.id !== employeeId);
+    fs.writeFile('src/data/employees.json', JSON.stringify(filteredEmployee), (err) => {
+      if (err) {
+        res.send('Employee cannot be deleted.');
+      } else {
+        res.send('Employee deleted.');
+      }
+    });
+  } else {
+    res.send('Employee cannot be deleted.');
   }
 });
 
