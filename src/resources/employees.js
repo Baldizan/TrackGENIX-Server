b455592635b47e643ed823f17d6ebe5e43929e7a
Eial Alalachvily <eialyafe@gmail.com>
2022-10-09 23:11:15 -0300

    TG: functionality create new employee done

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,7 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
-// const fs = require('fs');
-// const employees = require('../data/employees.json');
+const fs = require('fs');
+const employees = require('../data/employees.json');
 
 module.exports = router;
+
+router.post('/createEmployee', (req, res) => {
+  const newEmployee = req.body;
+  employees.push(newEmployee);
+  fs.writeFile('src/data/employee.jsno', JSON.stringify(employees), (err) => {
+    if (err) {
+      res.send('Cannot create new employee');
+    } else {
+      res.send('New employee created successfully');
+    }
+  });
+});
