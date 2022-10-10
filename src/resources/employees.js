56b29a75c5bb92b3d5bcb9582b19dbf38edd884a
Eial Alalachvily <eialyafe@gmail.com>
2022-10-10 10:32:52 -0300

    TG: functionality edit employee done

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -9,11 +9,30 @@ module.exports = router;
 router.post('/createEmployee', (req, res) => {
   const newEmployee = req.body;
   employees.push(newEmployee);
-  fs.writeFile('src/data/employee.jsno', JSON.stringify(employees), (err) => {
+  fs.writeFile('src/data/employees.json', JSON.stringify(employees), (err) => {
     if (err) {
       res.send('Cannot create new employee');
     } else {
       res.send('New employee created successfully');
+    }
+  });
+});
+
+router.put('/editEmployee', (req, res) => {
+  const employeeId = req.params.id;
+  const employeeToBeEdited = req.body;
+  let newEmployeesList = employees;
+  newEmployeesList = newEmployeesList.map((employee) => {
+    if (employee.id === employeeId) {
+      return employeeToBeEdited;
+    }
+    return employee;
+  });
+  fs.writeFile('src/data/employee.json', JSON.stringify(newEmployeesList), (err) => {
+    if (err) {
+      res.send('Cannot update employee');
+    } else {
+      res.send('Employee updated successfully');
     }
   });
 });
