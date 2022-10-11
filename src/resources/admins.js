966ddeee628c22321668f4f144dd33d4e39980aa
Maximiliano González <maxig.dev@gmail.com>
2022-10-11 00:05:05 -0300

    TG: Add edit function

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -18,3 +18,25 @@ router.post('/admins/add', (req, res) => {
     }
   });
 });
+
+// Edit admins
+router.put('/admins/edit', (req, res) => {
+  const editAdmin = req.body;
+  const adminId = Number(editAdmin.id);
+  const foundAdmin = admins.find((adminFilter) => adminFilter.id === adminId);
+  if (!foundAdmin) {
+    res.send('Admin not found');
+  } else if (JSON.stringify(editAdmin) === JSON.stringify(foundAdmin)) {
+    res.send('Admin not change, es igual pete');
+  } else {
+    const filterAdmin = admins.filter((adminFilter) => adminFilter.id !== editAdmin.id);
+    filterAdmin.push(editAdmin);
+    fs.writeFile('src/data/admins.json', JSON.stringify(filterAdmin), (err) => {
+      if (err) {
+        res.send('Cannot edit Admin');
+      } else {
+        res.send('Admin edited successfully');
+      }
+    });
+  }
+});
