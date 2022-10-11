04f102df049dfaf0703c7683ee178cc9b400ced1
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-10-11 14:37:41 -0300

    TG: add filters to get method

--- src/resources/employees.js
+++ src/resources/employees.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const fs = require('fs');
 const employee = require('../data/employees.json');
 
+router.get('/allEmployees', (req, res) => {
+  res.send(employee);
+});
+
 router.get('/getEmployee/:id', (req, res) => {
   const employeeId = req.params.id;
   const findEmployee = employee.find((emp) => emp.id === employeeId);
@@ -11,6 +15,26 @@ router.get('/getEmployee/:id', (req, res) => {
     res.send(findEmployee);
   } else {
     res.send('User not found.');
+  }
+});
+
+router.get('/getByTask/:task', (req, res) => {
+  const taskParam = req.params.task;
+  const filteredTask = employee.filter((t) => t.task === taskParam);
+  if (filteredTask.length > 0) {
+    res.send(filteredTask);
+  } else {
+    res.send('Task not found');
+  }
+});
+
+router.get('/getByProject/:projects/:project_id', (req, res) => {
+  const projectParam = req.params.projects[0].project_id;
+  const filteredProject = employee.filter((t) => t.projects.project_id === projectParam);
+  if (filteredProject.length > 0) {
+    res.send(filteredProject);
+  } else {
+    res.send('Project ID not found');
   }
 });
 
