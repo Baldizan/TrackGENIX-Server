756873f1dcf0713c0d2f2966e759890fe2e35024
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-10-11 21:18:17 -0300

    TG: add post method and filters with get

--- src/resources/time-sheets.js
+++ src/resources/time-sheets.js
@@ -1,5 +1,4 @@
 import express from 'express';
-
 import fs from 'fs';
 
 const router = express.Router();
@@ -7,6 +6,45 @@ const timeSheets = require('../data/time-sheets.json');
 
 router.get('/all', (req, res) => {
   res.send(timeSheets);
+});
+
+router.get('/get/:id', (req, res) => {
+  const timeSheetId = req.params.id;
+  const findTimeSheet = timeSheets.find((ts) => ts.id === timeSheetId);
+  if (findTimeSheet) {
+    res.send(findTimeSheet);
+  } else {
+    res.send('Time-Sheet not found.');
+  }
+});
+
+router.get('/getDate/:date', (req, res) => {
+  const timeSheetsDate = req.params.date;
+  const findTimeSheet = timeSheets.filter((d) => d.date === timeSheetsDate);
+  if (findTimeSheet.length > 0) {
+    res.send(findTimeSheet);
+  } else {
+    res.send('Time-sheet not found');
+  }
+});
+router.post('/create', (req, res) => {
+  const newTask = req.body.task;
+  const newDate = req.body.date;
+  const newDescription = req.body.description;
+  const newTimeSheet = {
+    id: new Date().getTime().toString().substring(6),
+    task: newTask,
+    date: newDate,
+    description: newDescription,
+  };
+  timeSheets.push(newTimeSheet);
+  fs.writeFile('src/data/time-sheets.json', JSON.stringify(timeSheets, null, 2), (err) => {
+    if (err) {
+      res.send('Error! cannot create Time-sheet');
+    } else {
+      res.send('Time-sheet was created');
+    }
+  });
 });
 
 router.delete('/delete/:id', (req, res) => {
