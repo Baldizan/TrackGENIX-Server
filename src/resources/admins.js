8af16d208b2a2b7da8a287d78fd21ca495cc3e31
Maximiliano González <maxig.dev@gmail.com>
2022-10-11 21:33:12 -0300

    TG: Update create function

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -6,9 +6,18 @@ const admins = require('../data/admins.json');
 
 module.exports = router;
 
-// Add admins
-router.post('/admins/add', (req, res) => {
-  const newAdmin = req.body;
+router.post('/add', (req, res) => {
+  const adminParam = req.body;
+  const newAdmin = {
+    id: adminParam.id = new Date().getTime().toString().substring(6),
+    first_name: adminParam.first_name ?? 'name undefined',
+    last_name: adminParam.last_name ?? 'last name undefined',
+    email: adminParam.email ?? 'email undefined',
+    password: adminParam.password ?? 'password undefined',
+    create_employee_id: adminParam.create_employee_id ?? 'not employees created',
+    delete_employee_id: adminParam.delete_employee_id ?? 'not employees delete',
+    edit_employee_id: adminParam.edit_employee_id ?? 'not employees edited',
+  };
   admins.push(newAdmin);
   fs.writeFile('src/data/admins.json', JSON.stringify(admins), (err) => {
     if (err) {
@@ -19,15 +28,14 @@ router.post('/admins/add', (req, res) => {
   });
 });
 
-// Edit admins
-router.put('/admins/edit', (req, res) => {
+router.put('/edit', (req, res) => {
   const editAdmin = req.body;
   const adminId = Number(editAdmin.id);
   const foundAdmin = admins.find((adminFilter) => adminFilter.id === adminId);
   if (!foundAdmin) {
     res.send('Admin not found');
   } else if (JSON.stringify(editAdmin) === JSON.stringify(foundAdmin)) {
-    res.send('Admin not change, es igual pete');
+    res.send('Admin not have data to change');
   } else {
     const filterAdmin = admins.filter((adminFilter) => adminFilter.id !== editAdmin.id);
     filterAdmin.push(editAdmin);
