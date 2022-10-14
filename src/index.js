87e9321ed528e154e5314148120af2b31cfbbbd4
Elias Bermudez <eliasbermudezxiii@gmail.com>
2022-10-14 11:10:51 -0300

    TG: Install Joi and Mongoose, created folders structure, and postman collection

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import express from 'express';
-import adminsRouter from './resources/admins';
-import taskRouter from './resources/tasks';
-import projectRouter from './resources/projects';
-import timeSheets from './resources/time-sheets';
-import employeeRouter from './resources/employees';
+import mongoose from 'mongoose';
+import adminsRouter from './controllers/admins';
+import taskRouter from './controllers/tasks';
+import projectRouter from './controllers/projects';
+import timeSheets from './controllers/time-sheets';
+import employeeRouter from './controllers/employees';
 
 const admins = require('./data/admins.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const MONGO_URL = "mongodb+srv://RadiumA:RadiumA@trackgenix.r6u6do6.mongodb.net/?retryWrites=true&w=majority"
 
 app.use(express.json());
 app.use('/admins', adminsRouter);
 app.use('/timesheets', timeSheets);
 app.use('/employees', employeeRouter);
 app.use('/projects', projectRouter);
+app.use('/tasks', taskRouter);
+
 app.get('/', async (req, res) => {
   res.send('Hello World!');
 });
@@ -25,8 +29,15 @@ app.get('/admins', (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-app.use('/tasks', taskRouter);
+mongoose.connect(
+  MONGO_URL, (error) => {
+    if (error) {
+      console.log('Fail conection to database', error);
+    } else{
+        console.log('Connected to database');
+        app.listen(port, () => {
+          console.log(`Example app listening on port ${port}`);
+        });
+    }
+  }
+);
