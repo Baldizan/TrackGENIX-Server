f8ae9475559e26e1965a0050178a5940fa7e2355
Maxi-SR <schmidtmaxi46@gmail.com>
2022-10-16 17:35:25 -0300

    TG: superAdmin schema created

---
+++ src/routes/superAdmins.js
@@ -0,0 +1,12 @@
+// import express from 'express';
+
+// const router = express.Router();
+
+// router
+//   .get('/',)
+//   .get('/:id')
+//   .post('/')
+//   .delete('/:id')
+//   .put('/:id')
+
+// export default router;
