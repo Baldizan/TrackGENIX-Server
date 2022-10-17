ad492cd927ddbb6ff12fe2d6998db109008eab5c
Maxi-SR <schmidtmaxi46@gmail.com>
2022-10-17 01:37:07 -0300

    TG: collections updated, create validation's done

--- src/routes/superAdmins.js
+++ src/routes/superAdmins.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import superAdminsController from '../controllers/superAdmins';
+import superAdminsValidation from '../validations/superAdmins';
 
 const router = express.Router();
 router
   .get('/', superAdminsController.getAllSuperAdmins)
   .get('/:id', superAdminsController.getAdminById)
-  .post('/', superAdminsController.createSuperAdmin)
+  .post('/', superAdminsValidation.validateCreation, superAdminsController.createSuperAdmin)
   .delete('/:id')
   .put('/:id');
 
---
+++ src/validations/superAdmins.js
@@ -0,0 +1,27 @@
+import Joi from 'joi';
+
+const validateCreation = (req, res, next) => {
+  const pattern = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
+  const superAdminValidation = Joi.object({
+    name: Joi.string().min(3).max(50).required(),
+    lastName: Joi.string().min(3).max(50).required(),
+    email: Joi.string().pattern(new RegExp(pattern)).required(),
+    password: Joi.string().alphanum().min(8).required(),
+  });
+
+  const validation = superAdminValidation.validateAsync(req.body);
+
+  if (validation.error) {
+    return res.status(400).json({
+      message: `There was an error: ${validation.error.details[0].message}`,
+      data: undefined,
+      error: true,
+    });
+  }
+
+  return next();
+};
+
+export default {
+  validateCreation,
+};
