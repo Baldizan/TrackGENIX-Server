183f53281bf71709f8995251f7ddd24f1950f603
Elias Bermudez <eliasbermudezxiii@gmail.com>
2022-10-18 00:33:42 -0300

    TG-11: Edit task controller and route add

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -13,38 +13,36 @@ const deleteTask = async (req, res) => {
   } catch (error) {
     return res.status(404).json({
       message: error,
+      data: undefined,
       error: true,
     });
   }
 };
 
-// router.put('/edit/:id', (req, res) => {
-//   const taskId = req.params.id;
-//   const foundTask = tasks.find((task) => task.id === taskId);
-//   if (foundTask) {
-//     const updateTask = req.body;
-//     tasks.forEach((task) => {
-//       if (task.id === taskId) {
-//         foundTask.name = updateTask.name ?? task.name;
-//         foundTask.description = updateTask.description ?? task.description;
-//         foundTask.project_id = updateTask.project_id ?? task.project_id;
-//         foundTask.hours = updateTask.hours ?? task.hours;
+const editTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Tasks.findByIdAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true },
+    );
 
-//         res.json({ msg: 'Task updated', task });
-//       }
-//     });
-//   } else {
-//     res.status(400).json({ msg: 'No tasks with the id' });
-//   }
-//   fs.writeFile('src/data/tasks.json', JSON.stringify(tasks, null, 2), (err) => {
-//     if (err) {
-//       res.send('Cannot update');
-//     } else {
-//       res.send('Task updated');
-//     }
-//   });
-// });
+    return res.status(200).json({
+      message: `Task with id ${id} edited.`,
+      data: result,
+      error: false,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error,
+      data: undefined,
+      error: true,
+    });
+  }
+};
 
-export default{
+export {
   deleteTask,
+  editTask,
 };
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { deleteTask } from '../controllers/tasks';
+import { deleteTask, editTask } from '../controllers/tasks';
 // import tasksValidation from '../validations/tasks';
 
 const router = express.Router();
 
 router.delete('/:id', deleteTask);
+
+router.put('/:id', editTask);
 
 export default router;
