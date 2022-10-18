ee3450931ec9f2b2f703c8346c88ee5dfcc443a3
Elias Bermudez <eliasbermudezxiii@gmail.com>
2022-10-18 00:59:32 -0300

    TG-11: Postman collection actualized

--- src/validations/tasks.js
+++ src/validations/tasks.js
@@ -21,25 +21,25 @@ const validateCreation = (req, res, next) => {
 };
 
 const validateEdit = (req, res, next) => {
-    const tasksEditValidation = Joi.object({
-        name: Joi.string().min(3).max(50),
-        description: Joi.string().min(3).max(50),
-        hours: Joi.number(),
-      });
-    
-      const validation = tasksEditValidation.validate(req.body);
-    
-      if (validation.error) {
-        return res.status(400).json({
-          message: `There was an error: ${validation.error.details[0].message}`,
-          data: undefined,
-          error: true,
-        });
-      }
-      return next();
-}
+  const tasksEditValidation = Joi.object({
+    name: Joi.string().min(3).max(50),
+    description: Joi.string().min(3).max(50),
+    hours: Joi.number(),
+  });
+
+  const validation = tasksEditValidation.validate(req.body);
+
+  if (validation.error) {
+    return res.status(400).json({
+      message: `There was an error: ${validation.error.details[0].message}`,
+      data: undefined,
+      error: true,
+    });
+  }
+  return next();
+};
 
 export {
   validateCreation,
-  validateEdit
+  validateEdit,
 };
