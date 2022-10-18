d58a846ee29933006c0cf518d538cf2f1a487465
ZGMauri <mauri1891.mm@gmail.com>
2022-10-18 14:24:26 -0300

    TG-3: add update employees

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -18,8 +18,26 @@ const deleteEmployees = async (req, res) => {
   }
 };
 
-const updateEmployees = async () => {
+const updateEmployees = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Employees.findByIdAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true },
+    );
 
+    return res.status(200).json({
+      message: `Employee with id ${id} edited`,
+      date: result,
+      error: false,
+    });
+  } catch (error) {
+    return res.json({
+      message: 'An error ocurred',
+      error,
+    });
+  }
 };
 
 export {
