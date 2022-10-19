d0bb31d4856e73dab1765a485371ee7d2572655b
Elias Bermudez <eliasbermudezxiii@gmail.com>
2022-10-18 22:35:30 -0300

    TG-10: Fix id status error code and make getAll more conditions to check

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -3,17 +3,18 @@ import Tasks from '../models/Tasks';
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Tasks.find(req.query);
-    if (tasks.length === 0) {
-      throw new Error('Task not found');
+    if (Object.keys(req.query).length !== 0 && tasks.length === 0) {
+      throw new Error('Tasks not found');
     }
+    const message = tasks.length ? 'Tasks found' : 'There are no tasks';
     return res.status(200).json({
-      message: 'Tasks found',
+      message,
       data: tasks,
       error: false,
     });
   } catch (error) {
     let status = 400;
-    if (error.message.includes('Task not found')) {
+    if (error.message.includes('Tasks not found')) {
       status = 404;
     }
     return res.status(status).json({
@@ -29,7 +30,7 @@ const getTaskById = async (req, res) => {
     const { id } = req.params;
     const tasks = await Tasks.findById(id);
     if (!tasks) {
-      throw new Error(`Task with id: ${id} not found`);
+      throw new Error('Task not found');
     }
     return res.status(200).json({
       message: 'Task found',
@@ -38,7 +39,7 @@ const getTaskById = async (req, res) => {
     });
   } catch (error) {
     let status = 400;
-    if (error.message.includes('Task with id:')) {
+    if (error.message.includes('Task not found')) {
       status = 404;
     }
     return res.status(status).json({
