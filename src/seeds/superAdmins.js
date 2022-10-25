41e1eb04ab7c15940267881585fa07a6f20bcd1a
ZGMauri <mauri1891.mm@gmail.com>
2022-10-25 19:35:58 -0300

    TG-3: add seed and test get method

---
+++ src/seeds/superAdmins.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: mongoose.Types.ObjectId('63519b632b611499e4a21401'),
+  name: 'superADM',
+  lastName: 'adminLn',
+  email: 'adminl@gmail.com',
+  password: 'aksdm122',
+}, {
+  _id: mongoose.Types.ObjectId('6351d60acf0f4e304906b2db'),
+  name: 'capiSuperAdmin',
+  lastName: 'adminLn',
+  email: 'adminl@gmail.com',
+  password: 'aksdm122',
+}, {
+  _id: mongoose.Types.ObjectId('6351d615cf0f4e304906b2dd'),
+  name: 'RaveSuperAdmin',
+  lastName: 'adminLn',
+  email: 'adminl@gmail.com',
+  password: 'aksdm122',
+}];
--- src/tests/superAdmins.test.js
+++ src/tests/superAdmins.test.js
@@ -1,5 +1,33 @@
-describe('superAdmin testings', () => {
-  test('superAdmin test 1', () => {
-    expect(5).toBe(5);
+import request from 'supertest';
+import app from '../app';
+import SuperAdmins from '../models/SuperAdmins';
+import superAdminsSeed from '../seeds/superAdmins';
+
+beforeAll(async () => {
+  await SuperAdmins.collection.insertMany(superAdminsSeed);
+});
+
+const correctId = '63519b632b611499e4a21401';
+const incorrectId = 'x';
+const notFoundId = '6357f056457ff7c72b2374d0';
+
+describe('GET /superAdmins', () => {
+  test('Should return status code 200, error false and more than one super admin', async () => {
+    const response = await request(app).get('/super-admins').send();
+    expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+  test('Should return status code 200 if found Id', async () => {
+    const response = await request(app).get(`/super-admins/${correctId}`).send();
+    expect(response.status).toBe(200);
+  });
+  test('Should return status code 400 if the super admin Id is incorrect', async () => {
+    const response = await request(app).get(`/super-admins/${incorrectId}`).send();
+    expect(response.status).toBe(400);
+  });
+  test('Should return status code 404 if not found super admin Id', async () => {
+    const response = await request(app).get(`/super-admins/${notFoundId}`).send();
+    expect(response.status).toBe(404);
   });
 });
