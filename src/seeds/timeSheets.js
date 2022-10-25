339405fa00579ecceac6f54848bb2abaeb043c13
Elias Bermudez <eliasbermudezxiii@gmail.com>
2022-10-24 23:09:41 -0300

    TG-4: Add timesheets seed data

---
+++ src/seeds/timeSheets.js
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: mongoose.Types.ObjectId('63573e59ca9eab60e9e9519e'),
+  description: 'Mocked timesheet with test prouposes',
+  date: '2022-10-24',
+  task: mongoose.Types.ObjectId('6350c0fc3ffdd3ec77f6a2ac'),
+  hours: 24,
+  project: mongoose.Types.ObjectId('634c973322e3ae91f635cf8f'),
+  employee: mongoose.Types.ObjectId('63571eb19298965ae634f74d'),
+}];
