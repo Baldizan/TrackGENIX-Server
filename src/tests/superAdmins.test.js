446c013617603f1fc0b00d249255d3e7e480c094
ZGMauri <mauri1891.mm@gmail.com>
2022-10-25 22:25:22 -0300

    TG-3: make corrections in all methods

--- src/tests/superAdmins.test.js
+++ src/tests/superAdmins.test.js
@@ -14,6 +14,10 @@ const dataSuperAdmin = {
   password: 'aksdm122',
 };
 
+const invalidDataSuperAdmin = {
+  name: 123456789,
+};
+
 const correctId = '63519b632b611499e4a21401';
 const incorrectId = 'x';
 const notFoundId = '6357f056457ff7c72b2374d0';
@@ -54,6 +58,7 @@ describe('POST /superAdmins', () => {
   test('Should not create an super admin', async () => {
     const response = await request(app).post('/super-admins').send();
     expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
   });
 });
 
@@ -61,14 +66,17 @@ describe('DELETE /superAdmins', () => {
   test('Should delete an super super admin', async () => {
     const response = await request(app).delete(`/super-admins/${correctId}`).send();
     expect(response.status).toBe(204);
+    expect(response.body.error).toBeFalsy();
   });
   test('Should return status code 400 if the super admin Id is incorrect', async () => {
     const response = await request(app).delete(`/super-admins/${incorrectId}`).send();
     expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
   });
   test('Should return status code 404 if not found super admin Id', async () => {
     const response = await request(app).delete(`/super-admins/${notFoundId}`).send();
     expect(response.status).toBe(404);
+    expect(response.body.error).toBeTruthy();
   });
 });
 
@@ -77,13 +85,22 @@ describe('PUT /superAdmins', () => {
     // eslint-disable-next-line no-underscore-dangle
     const response = await request(app).put(`/super-admins/${superAdminsSeed[2]._id}`).send();
     expect(response.status).toBe(200);
+    expect(response.body.error).toBeFalsy();
   });
   test('Should return status code 400 if the super admin Id is incorrect', async () => {
     const response = await request(app).put(`/super-admins/${incorrectId}`).send();
     expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
   });
   test('Should return status code 404 if not found super admin Id', async () => {
     const response = await request(app).put(`/super-admins/${notFoundId}`).send();
     expect(response.status).toBe(404);
+    expect(response.body.error).toBeTruthy();
+  });
+  test('Should return status code 400 if data is invalid', async () => {
+    const response = await request(app).put(`/super-admins/${correctId}`).send(invalidDataSuperAdmin);
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
   });
 });
