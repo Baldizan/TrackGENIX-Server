7209227a06001e686924834e14eaef9914b7d265
Martin Raveglia <martin.raveglia@radiumrocket.com>
2022-10-26 23:53:05 -0300

    TG-3: Update SuperAdmin UTs

--- src/tests/superAdmins.test.js
+++ src/tests/superAdmins.test.js
@@ -7,6 +7,12 @@ beforeAll(async () => {
   await SuperAdmins.collection.insertMany(superAdminsSeed);
 });
 
+const superAdminsSeedWithIdStringified = superAdminsSeed.map((superAdmin) => ({
+  ...superAdmin,
+  // eslint-disable-next-line no-underscore-dangle
+  _id: superAdmin._id.toString(),
+}));
+
 const dataSuperAdmin = {
   name: 'superADM',
   lastName: 'adminLn',
@@ -27,7 +33,8 @@ describe('GET /superAdmins', () => {
     const response = await request(app).get('/super-admins').send();
     expect(response.status).toBe(200);
     expect(response.body.error).toBeFalsy();
-    expect(response.body.data.length).toBeGreaterThan(0);
+    expect(response.body.message).toEqual('Super admins found');
+    expect(response.body.data).toEqual(superAdminsSeedWithIdStringified);
   });
 });
 
@@ -36,16 +43,26 @@ describe('GET /superAdmins/:id', () => {
     const response = await request(app).get(`/super-admins/${correctId}`).send();
     expect(response.status).toBe(200);
     expect(response.body.error).toBeFalsy();
+    const superAdminWithCorrectId = superAdminsSeedWithIdStringified.find(
+      // eslint-disable-next-line no-underscore-dangle
+      (superAdmin) => superAdmin._id === correctId,
+    );
+    expect(response.body.data).toEqual(superAdminWithCorrectId);
+    expect(response.body.message).toEqual(`Super admin with id: ${correctId} found`);
   });
   test('Should return status code 400 if the super admin Id is incorrect', async () => {
     const response = await request(app).get(`/super-admins/${incorrectId}`).send();
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual('CastError: Cast to ObjectId failed for value "x" (type string) at path "_id" for model "SuperAdmin"');
   });
   test('Should return status code 404 if not found super admin Id', async () => {
     const response = await request(app).get(`/super-admins/${notFoundId}`).send();
     expect(response.status).toBe(404);
     expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual('Error: Super admin not found');
   });
 });
 
@@ -54,35 +71,44 @@ describe('POST /superAdmins', () => {
     const response = await request(app).post('/super-admins').send(dataSuperAdmin);
     expect(response.status).toBe(201);
     expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toEqual('Super admin created succesfully');
+    expect(response.body.data).toMatchObject(dataSuperAdmin);
   });
   test('Should not create an super admin if has no data', async () => {
     const response = await request(app).post('/super-admins').send();
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual('There was an error: "name" is required. "lastName" is required. "email" is required. "password" is required');
   });
   test('Should not create an super admin if data is invalid', async () => {
     const response = await request(app).post('/super-admins').send(invalidDataSuperAdmin);
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
     expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual('There was an error: "name" must be a string. "lastName" is required. "email" is required. "password" is required');
   });
 });
 
-describe('DELETE /superAdmins', () => {
+describe('DELETE /superAdmins/:id', () => {
   test('Should delete an super super admin', async () => {
     const response = await request(app).delete(`/super-admins/${correctId}`).send();
     expect(response.status).toBe(204);
-    expect(response.body.error).toBeFalsy();
+    expect(response.body).toEqual({});
   });
   test('Should return status code 400 if the super admin Id is incorrect', async () => {
     const response = await request(app).delete(`/super-admins/${incorrectId}`).send();
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual(`CastError: Cast to ObjectId failed for value "${incorrectId}" (type string) at path "_id" for model "SuperAdmin"`);
   });
   test('Should return status code 404 if not found super admin Id', async () => {
     const response = await request(app).delete(`/super-admins/${notFoundId}`).send();
     expect(response.status).toBe(404);
     expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual("Error: Super admin doesn't exist");
   });
 });
 
@@ -92,21 +118,29 @@ describe('PUT /superAdmins', () => {
     const response = await request(app).put(`/super-admins/${superAdminsSeed[2]._id}`).send();
     expect(response.status).toBe(200);
     expect(response.body.error).toBeFalsy();
+    expect(response.body.message).toEqual('Super admin updated');
+    expect(response.body.data).toMatchObject(superAdminsSeed[2]);
   });
   test('Should return status code 400 if the super admin Id is incorrect', async () => {
     const response = await request(app).put(`/super-admins/${incorrectId}`).send();
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual(`CastError: Cast to ObjectId failed for value "${incorrectId}" (type string) at path "_id" for model "SuperAdmin"`);
   });
   test('Should return status code 404 if not found super admin Id', async () => {
     const response = await request(app).put(`/super-admins/${notFoundId}`).send();
     expect(response.status).toBe(404);
     expect(response.body.error).toBeTruthy();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual("Error: Super admin doesn't exist");
   });
   test('Should return status code 400 if data is invalid', async () => {
     const response = await request(app).put(`/super-admins/${correctId}`).send(invalidDataSuperAdmin);
     expect(response.status).toBe(400);
     expect(response.body.error).toBeTruthy();
     expect(response.body.data).toBeUndefined();
+    expect(response.body.data).toBeUndefined();
+    expect(response.body.message).toEqual('There was an error: "name" must be a string');
   });
 });
