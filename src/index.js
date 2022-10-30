8fc851e4fa75038f0dc270c11f521d4d4eece4ef
Guido Gentile <guido.gentile.1995@gmail.com>
2022-10-30 20:41:05 -0300

    TG-5: added cors

--- src/index.js
+++ src/index.js
@@ -7,10 +7,13 @@ const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DATABASE_URL, (error) => {
   if (error) {
+    // eslint-disable-next-line
     console.log('Fail conection to database 🔴 ', error);
   } else {
+    // eslint-disable-next-line
     console.log('Connected to database 🟢');
     app.listen(port, () => {
+      // eslint-disable-next-line
       console.log(`App listening on port ${port}`);
     });
   }
