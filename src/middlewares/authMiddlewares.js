1c3c0a8070e4cdf9c6278ff4d9ffadb5650fa98f
Guido Gentile <guido.gentile.1995@gmail.com>
2022-11-26 22:10:54 -0300

    TG-93: Created authMiddlewares file

---
+++ src/middlewares/authMiddlewares.js
@@ -0,0 +1,20 @@
+import firebase from '../helpers/firebase';
+
+const checkAuth = (roles) => async (req, res, next) => {
+  try {
+    const { token } = req.headers;
+    if (!token) {
+      return res.status(400).json({ message: 'No token provided.' });
+    }
+    const user = await firebase.auth().verifyIdToken(token);
+    if (!roles.includes(user.role)) {
+      throw new Error('Invalid role.');
+    }
+    req.firebaseUid = user.uid;
+    return next();
+  } catch (error) {
+    return res.status(401).json({ message: error.toString() });
+  }
+};
+
+export default checkAuth;
